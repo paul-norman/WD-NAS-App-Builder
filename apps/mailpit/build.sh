1f069c84efe4167e74eb99227fd7c42df08fb129
Paul Norman <1172942+paul-norman@users.noreply.github.com>
2024-12-28 12:20:44 +0000

    Add Mailpit

---
+++ apps/mailpit/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+. "../build_helpers.sh"
+
+# Some models use AMD64 architecture and others use ARM
+declare -A MODELS
+MODELS[amd64]="MyCloudPR4100 MyCloudPR2100 WDMyCloudDL4100 WDMyCloudDL2100"
+MODELS[arm]="WDCloud WDMyCloud WDMyCloudMirror WDMyCloudEX4100 WDMyCloudEX2100 MyCloudEX2Ultra"
+
+for ARCH in "${!MODELS[@]}"; do
+	# Make a directory for the files
+	mkdir -p binaries
+	cd binaries
+
+	MP_REPO="https://github.com/axllent/mailpit/releases/download/v${APKG_VERSION}/mailpit-linux-${ARCH}.tar.gz"
+
+	# Download and extract the right version of Mailpit
+	echo -e "\nDownloading: ${MP_REPO}"
+	wget ${MP_REPO} -O mailpit.tar.gz
+
+	# Extract the data
+	if [ -f mailpit.tar.gz ]; then
+		tar -xf mailpit.tar.gz
+		rm mailpit.tar.gz
+		DIR=$(ls | head -n 1)
+		if [ -d ${DIR} ] && [ "${DIR}" != "" ]; then
+			mv ${DIR}/* .
+			rm -rf ${DIR}
+		fi
+	else
+		abort "$(pwd)/mailpit.tar.gz could not be downloaded"
+	fi
+
+	cd ../
+
+	# Build the archive for all models of this architecture
+	build ${MODELS[${ARCH}]} ${ARCH}
+	
+	# Clean up any mess
+	rm -rf binaries
+done
+
+# Cleanup
+. "../build_helpers.sh"
